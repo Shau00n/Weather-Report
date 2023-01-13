ffcc407ddf03ba1edb19ddb8d477b44706a13663
Shau00n <flankeeeer7@gmail.com>
2023-01-13 16:24:40 +0900

    各種テーブルのマイグレートファイルの作成

---
+++ database/migrations/2022_12_22_011858_create_citycodes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citycodes', function (Blueprint $table) {
+            $table->id();
+            $table->text('prefecture_name');
+            $table->text('city_name');
+            $table->integer('latitude');
+            $table->integer('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citycodes');
+    }
+};
---
+++ database/migrations/2022_12_22_012152_create_bodies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bodies', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('user_id')->constrained('users');
+            // $table->text('codinate_name');
+            // $table->integer('clo_val');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bodies');
+    }
+};
+
+// ログイン関連ナシなら６．５割
+// ログインなら
